64c109f4925875899ea6d979d03dfe53837fcf1b
Matheus Meneses <matheusmeneses134@gmail.com>
2026-08-10 18:27:52 -0300

    Deploy Streamlit

--- streamlit_app/utils/api.py
+++ streamlit_app/utils/api.py
@@ -1,36 +1,39 @@
 import requests
-import time
 
 URL_API = "https://openhealth-analytics.onrender.com"
 
-def _get_com_retry(endpoint, tentativas=3, espera=15):
-    for i in range(tentativas):
-        try:
-            response = requests.get(f"{URL_API}{endpoint}", timeout=60)
-            return response.json()
-        except requests.exceptions.ConnectionError:
-            if i < tentativas - 1:
-                time.sleep(espera)
-            else:
-                raise
 
 def get_casos_por_mes():
-    return _get_com_retry("/casos-por-mes")
+    
+    response = requests.get(f"{URL_API}/casos-por-mes")
+    return response.json()
 
 def get_casos_por_uf():
-    return _get_com_retry("/casos-por-uf")
+    
+    response = requests.get(f"{URL_API}/casos-por-uf")
+    return response.json()
 
 def get_classificacao_por_uf():
-    return _get_com_retry("/classificacao-casos-por-uf")
+    
+     response = requests.get(f"{URL_API}/classificacao-casos-por-uf")
+     return response.json()
 
 def get_taxa_cura_uf():
-    return _get_com_retry("/taxa-cura-por-uf")
+    
+    response = requests.get(f"{URL_API}/taxa-cura-por-uf")
+    return response.json()
 
 def get_casos_regiao():
-    return _get_com_retry("/casos-por-regiao")
+    
+    response = requests.get(f"{URL_API}/casos-por-regiao")
+    return response.json()
 
 def get_gravidade_regiao():
-    return _get_com_retry("/gravidade-por-regiao")
+    
+    response = requests.get(f"{URL_API}/gravidade-por-regiao")
+    return response.json()
 
 def get_indicador_clima_uf():
-    return _get_com_retry("/indicadores-clima-por-uf")
+    
+    response = requests.get(f"{URL_API}/indicadores-clima-por-uf")
+    return response.json()
