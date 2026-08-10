28d915070a9ba1fb4161a1874bd832fc416e63f7
Matheus Meneses <matheusmeneses134@gmail.com>
2026-08-10 18:11:41 -0300

    Deploy API

--- app/database/db.py
+++ app/database/db.py
@@ -7,18 +7,18 @@ from sqlalchemy.orm import sessionmaker
 logger = log.getLogger(__name__)
 load_dotenv()
 
-user = os.getenv("POSTGRES_USER")
-password = os.getenv("POSTGRES_PASSWORD")
-host = os.getenv("POSTGRES_HOST")
-port = os.getenv("POSTGRES_PORT")
-dbname = os.getenv("POSTGRES_DB")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASS")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+dbname = os.getenv("DB_NAME")
 
 if not all([user, password, host, port, dbname]):
     logger.error("Variaveis não definidas no arquivo .env")
     raise ValueError("Variaveis não definidas")
 
 try:
-    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}?sslmode=require&channel_binding=require"
     engine = create_engine(url, pool_pre_ping=True)
     SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind= engine)
     logger.info("✅")
